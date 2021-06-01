efb1a23c9cceae204516d4eeb946e612686f54d2
hvarts74 <80119260+hvarts74@users.noreply.github.com>
2021-06-01 18:27:33 +0500

    Delete models directory

--- models/post.js
+++
@@ -1,8 +0,0 @@
-const { Schema, model } = require('mongoose')
-
-const schema = new Schema({
-  title: {type: String, required: true},
-  descr: {type: String, required: true}
-})
-
-module.exports = model('Post', schema)
