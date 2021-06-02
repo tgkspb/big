af04c9bed80995235f15f8abb9b6073f1775b78d
tgkspb <73022272+tgkspb@users.noreply.github.com>
2021-06-02 13:35:42 +0300

    Update post.js

--- models/post.js
+++ models/post.js
@@ -3,7 +3,7 @@ const { Schema, model } = require('mongoose')
 const schema = new Schema({
   id: {type: Number, required: true},
   title: {type: String, required: true},
-  descr: {type: String, required: true}
+  imgurl: {type: String, required: true}
 })
 
 module.exports = model('Post', schema)
